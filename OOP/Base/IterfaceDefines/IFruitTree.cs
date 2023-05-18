6e651f65f7eac2c79415aab54446230f4ba74242
Alex H Ding <Alex H Ding@cn.asia.ad.pwcinternal.com>
2023-05-18 15:32:15 +0800

    1. Test the oop minds programming.

---
+++ OOP/Base/IterfaceDefines/IFruitTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.OOP.Base.IterfaceDefines
+{
+    /// <summary>
+    /// 果树的行为
+    /// </summary>
+    public interface IFruitTree
+    {
+        /// <summary>
+        /// 开花
+        /// </summary>
+        void Flower();
+
+        /// <summary>
+        /// 结果子
+        /// </summary>
+        void BearFruit();
+    }
+}
---
+++ OOP/Base/IterfaceDefines/IOrnamentalTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.OOP.Base.IterfaceDefines
+{
+
+    /// <summary>
+    /// 接口是对对象行为的抽象
+    /// 观赏树的行为
+    /// </summary>
+    public interface IOrnamentalTree
+    {
+        /// <summary>
+        /// 快速生长
+        /// </summary>
+        void GrowFaster();
+
+        /// <summary>
+        /// 颜色改变
+        /// </summary>
+        void ChangeColor();
+    }
+}
