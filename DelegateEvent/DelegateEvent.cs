55d6c1afa459b532294fa16d4ed6386e607ad1c6
Alex H Ding <>
2023-05-23 17:47:27 +0800

    add a test file

---
+++ DelegateEvent/DelegateEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DesignPattern.DelegateEvent
+{
+	public class DelegateEvent
+	{
+
+		public delegate void OnGrantHandler();
+		public delegate void OnRefuseHandler();
+
+		public event OnGrantHandler OnGrantEvent;
+		public event OnGrantHandler OnRefuseEvent;
+
+		/*public DelegateEvent()
+		{
+			SetEventHandler();
+        }*/
+
+		private void SetEventHandler()
+		{
+			OnGrantEvent += OnGrant;
+            OnRefuseEvent += OnRefuse;
+        }
+
+		private void OnGrant()
+		{
+            Console.WriteLine("OnGrant");
+        }
+
+        private void OnRefuse()
+        {
+			Console.WriteLine("OnRefuse");
+        }
+
+    }
+}
+
